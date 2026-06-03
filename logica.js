bb67c2df975b6f8536080a4af09935405e104435
unknown <axel123rry@gmail.com>
2026-06-03 15:56:17 -0600

    hola

---
+++ logica.js
@@ -0,0 +1,14 @@
+let uno=document.getElementById("uno")
+let dos=document.getElementById("dos")
+
+uno.addEventListener("click",suma)
+
+function suma(){
+    window.location.href= "/contador/hola_1.html"
+}
+
+dos.addEventListener("click",resta)
+
+function resta(){
+    window.location.href= "/nombre/hola_2.html"
+}
---
+++ nombre/logica.js
@@ -0,0 +1,7 @@
+let b=document.getElementById("b")
+
+b.addEventListener("click",suma)
+
+function suma() {
+    window.location.href = "/index.html"
+}
